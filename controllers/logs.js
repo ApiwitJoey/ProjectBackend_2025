90f305e93f02ea27ad34e2327710c3cd3eefa9e6
ApiwitJoey <joeyapiwitch@gmail.com>
2025-03-02 14:31:17 +0700

    Add populate in log

--- controllers/logs.js
+++ controllers/logs.js
@@ -2,7 +2,20 @@ const Log = require('../models/Log');
 
 exports.getLogs = async (req, res, next) => {
     try {
-        const logs = await Log.find().populate('user', 'name email').populate('reserve', 'rDate');
+        const logs = await Log.find()
+            .populate({
+                path: 'user',
+                select: 'name email role' // Include user role
+            })
+            .populate({
+                path: 'reserve',
+                populate: {
+                    path: 'campground', // Populate campground details
+                    select: 'name province telephone'
+                },
+                select: 'rDate user' // Include reservation date and user
+            });
+
         res.status(200).json({ success: true, count: logs.length, data: logs });
     } catch (error) {
         console.log(error);
